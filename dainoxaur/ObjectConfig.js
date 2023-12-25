0a054733db0fcf8c90018ff323ee9def6d39fd6b
3vil3y3 <101619460+evileye2002@users.noreply.github.com>
2023-12-26 00:34:07 +0700

    Delete dainoxaur directory

--- dainoxaur/ObjectConfig.js
+++
@@ -1,9 +0,0 @@
-import Sprite from "../src/Sprites.js";
-
-export default class ObjectConfig {
-  constructor(canvasID, sprite = null) {
-    this.canvas = document.getElementById(canvasID);
-    this.ctx = this.canvas.getContext("2d");
-    this.sprite = sprite != null ? new Sprite(sprite) : null;
-  }
-}
